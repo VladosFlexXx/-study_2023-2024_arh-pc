2e99701b3270efc3a2b5ed8cca0e5aa9c80b4eda
Владислав Лобанов <v.lobanov.o89@gmail.com>
2023-12-08 17:37:17 +0300

    1

--- lab06/lab6-1.asm
+++ lab06/lab6-1.asm
@@ -3,13 +3,12 @@ SECTION .bss
 buf1: RESB 80
 SECTION .text
 GLOBAL _start
- _start:
-  mov eax,'6'
- mov ebx,'4'
- add eax,ebx
- mov [buf1],eax
- mov eax,buf1
- call sprintLF
- 
- call quit
- 
+_start:
+mov eax,6
+mov ebx,4
+add eax,ebx
+mov [buf1],eax
+mov eax,buf1
+call sprintLF
+call quit
+
